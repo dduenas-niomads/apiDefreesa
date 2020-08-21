ba900b13de2aa5e330a016fc0294d96eb7055817
daniel <dduenas@tumipos.com>
2020-08-20 23:59:25 -0500

    Fix: 3.1 orders

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -49,5 +49,5 @@ Route::prefix('/products')->group(function() {
 Route::prefix('/orders')->group(function() {
     Route::middleware('auth:api')->get('/', 'Api\v1\OrderController@index');
     Route::middleware('auth:api')->post('/', 'Api\v1\OrderController@store');
-    Route::middleware('auth:api')->getById('/', 'Api\v1\OrderController@show');
+    Route::middleware('auth:api')->get('/{id}', 'Api\v1\OrderController@show');
 });
