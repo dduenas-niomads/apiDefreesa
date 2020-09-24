15adb0ab0af3a7534c0ef3db918b893e2f34c5ae
daniel <dduenas@tumipos.com>
2020-09-23 22:05:45 -0500

    F

--- app/Http/Controllers/Api/v1/CategoryController.php
+++ app/Http/Controllers/Api/v1/CategoryController.php
@@ -20,7 +20,8 @@ class CategoryController extends Controller
         $user = Auth::user();
         if (!is_null($user)) {
             $params = $request->all();
-            $categories = Category::whereNull('deleted_at');
+            $categories = Category::whereNull('deleted_at')
+                ->where('flag_active', true)
             if (isset($params['search']) && !is_null($params['search'])) {
                 $key = $params['search'];
                 $categories = $categories->where(function($query) use ($key){
